2156a36e10a821bb5ae7810541fecc4be0f90f49
Adnan Sami Pavel <adnansamipavel@gmail.com>
2026-04-04 20:59:02 +0600

    added type hinting and few types of api operations

--- src/app.py
+++ src/app.py
@@ -1,22 +1,38 @@
-from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException, status
 
-textPost = ["This is my first post", "This is my second post", "This is my third post"]
 
-@app.get("/name")
-def my_name():
-    return {"name": "Adnan Sami Pavel", "age": 27, "country": "Bangladesh"} 
+app = FastAPI() # type: ignore
 
-@app.get("/about")
-def about_me():
+textPost: list[str] = ["This is my first post", "This is my second post", "This is my third post"]
+
+allPosts: dict[int, dict[str, str]] = {1: {"title": "First Post", "content": "This is the content of the first post"},
+                                       2: {"title": "Second Post", "content": "This is the content of the second post"},
+                                       3: {"title": "Third Post", "content": "This is the content of the third post"}}
+
+@app.get("/name") # type: ignore
+def my_name() -> dict[str, int | str]:
+    return {
+        "name": "Adnan Sami Pavel", 
+            "age": 27, 
+            "country": "Bangladesh"
+            } 
+
+@app.get("/about") # type: ignore
+def about_me() -> dict[str, int | str | list[str]]:
     return {"name": "Adnan Sami Pavel", "age": 27, "country": "Bangladesh", "hobbies": ["coding", "traveling", "cooking"]}
 
-@app.get("/posts")
-def my_posts():
+@app.get("/posts") # type: ignore
+def my_posts() -> dict[str, list[str]]:
     return {"posts": textPost }
 
-@app.post("/post-text")
-def post_text():
+@app.post("/post-text") # type: ignore
+def post_text() -> list[str]:
     textPost[1] = "These are the text that I want to post"
     return textPost
+
+@app.get("/posts/{post_id}") # type: ignore
+def get_post(post_id: int) -> dict[str, str]:
+    if post_id not in allPosts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post id: {post_id} not found") # type: ignore
+    return allPosts[post_id] # type: ignore
