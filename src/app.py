db0922c1aa567302197db502fddf8413f97cd420
Adnan Sami Pavel <adnansamipavel@gmail.com>
2026-04-04 19:34:15 +0600

    Added uvicorn server configuration to main.py and created a test.http file for testing the API endpoints. Also added a schema.json file for defining the data models used in the API.

--- src/app.py
+++ src/app.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+textPost = ["This is my first post", "This is my second post", "This is my third post"]
+
 @app.get("/name")
 def my_name():
     return {"name": "Adnan Sami Pavel", "age": 27, "country": "Bangladesh"} 
+
+@app.get("/about")
+def about_me():
+    return {"name": "Adnan Sami Pavel", "age": 27, "country": "Bangladesh", "hobbies": ["coding", "traveling", "cooking"]}
+
+@app.get("/posts")
+def my_posts():
+    return {"posts": textPost }
+
+@app.post("/post-text")
+def post_text():
+    textPost[1] = "These are the text that I want to post"
+    return textPost
